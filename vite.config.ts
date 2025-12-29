a56d37d16bba39f2ce36959828f0619e512fb582
Ahmad Sadaqat <ahmadsadaqat6161@gmail.com>
2025-12-30 02:03:20 +0500

    config: Add relative base path to Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: './',
   root: 'src/renderer',
   build: {
     outDir: '../../dist',
